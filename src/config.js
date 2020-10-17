d477d8c09dcd4f4e4b92cf0df0d8ec8cad639603
Minh Nguyen <tmnguyen1403@gmail.com>
2020-10-16 19:13:52 -0500

    10/16/2020 Setup aws-amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-minhmap",
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://5ug3p9ysil.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_SJU0ZgzR1",
+        APP_CLIENT_ID: "9at7qbtmqkc4cp4b4072ft9s8",
+        IDENTITY_POOL_ID: "us-east-1:6ae2cab3-c12c-42d7-9811-a38c93e0935a"
+    }
+};
